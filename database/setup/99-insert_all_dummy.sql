0618c59b05ed9ea6f3e0d2d67eb71f1d5e7f22ce
Renard Tumbokon <rentumbokon@gmail.com>
2019-10-12 22:07:50 -0400

    Update dummy sql file

--- database/setup/99-insert_all_dummy.sql
+++ database/setup/99-insert_all_dummy.sql
@@ -1,5 +1,5 @@
 DELETE FROM `user`;
-INSERT INTO `user` (user_id, username, password, first_name, last_name, alias, email, country, state, city, profile_image, game, game_role, about, resume_pdf, created_date, last_updated_date);
+INSERT INTO `user` (user_id, username, password, first_name, last_name, alias, email, country, state, city, profile_image, game, game_role, about, resume_pdf, created_date, last_updated_date)
 VALUES 
 	(1,'kerblaster','password','Renard','Tumbokon','kerblaster','kerblaster@hotmail.com','United States','NJ','Cranford',null,'Fortnite','Solo','Dabs on kids then flosses on them', null, NOW(),NOW()),
 	(2,'pudgypanda','password','Mike','Serpico','PudgyPanda','pudgypanda@hotmail.com','United States','NJ','Freehold',null,'SSBM','Ice Climbers','I wobble', null, NOW(),NOW()),
